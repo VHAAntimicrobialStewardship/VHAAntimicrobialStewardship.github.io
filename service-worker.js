9a2ef8aa0dcc25389c160033bbb3404aa29533fd
Andrew Holbrook <andrew.holbrook@va.gov>
2026-07-27 13:03:06 -0700

    working on network-first service worker logic

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const APP_PREFIX = 'CDSS_';
-const VERSION = '1.265'; // Update the version when you make changes
+const VERSION = '1.266'; // Update the version when you make changes
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const URLS = [
@@ -24,43 +24,43 @@ const URLS = [
   '/stations/001-TestStation/SinusitisTestOMJSON.json',
   '/stations/001-TestStation/TestStationODJSON.json',
 
-  '/stations/618-Minneapolis/Minneapolis.txml',
-  '/stations/618-Minneapolis/MinneapolisCDSS.html',
-  '/stations/618-Minneapolis/MinneapolisItemLinks.json',
-  '/stations/618-Minneapolis/MinneapolisOMJSON.json',
-  '/stations/618-Minneapolis/MinneapolisODJSON.json',
+  '/stations/437-Fargo/FargoCDSS.html',
+  '/stations/437-Fargo/FargoItemLinks.json',
+  '/stations/437-Fargo/FargoOMJSON.json',
+  '/stations/437-Fargo/FargoODJSON.json',
+
+  '/stations/438-SiouxFalls/SiouxFalls.txml',
+  '/stations/438-SiouxFalls/SiouxFallsCDSS.html',
+  '/stations/438-SiouxFalls/SiouxFallsOMJSON.json',
+  '/stations/438-SiouxFalls/SiouxFallsODJSON.json',
+
+  '/stations/442-Cheyenne/Cheyenne.txml',
+  '/stations/442-Cheyenne/CheyenneCDSS.html',
+  '/stations/442-Cheyenne/CheyenneOMJSON.json',
+  '/stations/442-Cheyenne/CheyenneODJSON.json',
 
   '/stations/568-BlackHills/BlackHills.txml',
   '/stations/568-BlackHills/BlackHillsCDSS.html',
   '/stations/568-BlackHills/BlackHillsOMJSON.json',
   '/stations/568-BlackHills/BlackHillsODJSON.json',
-    
-  '/stations/636A6-DesMoines/DesMoines.txml',
-  '/stations/636A6-DesMoines/DesMoinesCDSS.html',
-  '/stations/636A6-DesMoines/DesMoinesItemLinks.json',
-  '/stations/636A6-DesMoines/DesMoinesOMJSON.json',
-  '/stations/636A6-DesMoines/DesMoinesODJSON.json',
-      
-  '/stations/437-Fargo/FargoCDSS.html',
-  '/stations/437-Fargo/FargoItemLinks.json',
-  '/stations/437-Fargo/FargoOMJSON.json',
-  '/stations/437-Fargo/FargoODJSON.json',
-  
+
+  '/stations/618-Minneapolis/Minneapolis.txml',
+  '/stations/618-Minneapolis/MinneapolisCDSS.html',
+  '/stations/618-Minneapolis/MinneapolisOMJSON.json',
+  '/stations/618-Minneapolis/MinneapolisODJSON.json',
+
   '/stations/636-Omaha/Omaha.txml',
   '/stations/636-Omaha/OmahaCDSS.html',
-  '/stations/636-Omaha/OmahaItemLinks.json',
   '/stations/636-Omaha/OmahaOMJSON.json',
   '/stations/636-Omaha/OmahaODJSON.json',
-  
-  '/stations/438-SiouxFalls/SiouxFalls.txml',
-  '/stations/438-SiouxFalls/SiouxFallsCDSS.html',
-  '/stations/438-SiouxFalls/SiouxFallsItemLinks.json',
-  '/stations/438-SiouxFalls/SiouxFallsOMJSON.json',
-  '/stations/438-SiouxFalls/SiouxFallsODJSON.json',
+
+  '/stations/636A6-DesMoines/DesMoines.txml',
+  '/stations/636A6-DesMoines/DesMoinesCDSS.html',
+  '/stations/636A6-DesMoines/DesMoinesOMJSON.json',
+  '/stations/636A6-DesMoines/DesMoinesODJSON.json',
 
   '/stations/656-StCloud/StCloud.txml',
   '/stations/656-StCloud/StCloudCDSS.html',
-  '/stations/656-StCloud/StCloudItemLinks.json',
   '/stations/656-StCloud/StCloudOMJSON.json',
   '/stations/656-StCloud/StCloudODJSON.json',
 
