2fe7f2eff9c6c2f08239704ca6138fc375a0791e
Andrew Holbrook <andrew.holbrook@va.gov>
2026-07-27 13:19:14 -0700

    bumping service worker for link changes to CDSSLogo and VASeal images in TestStationCDSS.html

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const APP_PREFIX = 'CDSS_';
-const VERSION = '1.266'; // Update the version when you make changes
+const VERSION = '1.267'; // Update the version when you make changes
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const URLS = [
